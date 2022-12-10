64734ed25a71c91c6336c6d20724573c4c3e9a37
raquelcdcneris <raquelcdcavalcante@gmail.com>
2022-12-09 22:55:22 -0500

    Update

--- FinalProject_OndeEstaGal/scripts/main.js
+++ FinalProject_OndeEstaGal/scripts/main.js
@@ -2,3 +2,9 @@ function myFunction() {
   var popup = document.getElementById("myPopup");
   popup.classList.toggle("show");
 }
+
+document.querySelector("#1_DivinoMaravilhoso_GAL.svg").onclick = function(e) {
+  e.preventDefault();
+  const audio = new Audio("assets/tunes/DivinoMaravilhoso.mp3");
+  audio.play();
+}
